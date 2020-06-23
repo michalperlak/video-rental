dcbdde7eec96fa570e1d3dd5126722db064122c9
Michal Perlak <michalperlak@windowslive.com>
2020-06-24 00:11:37 +0200

    Add tests for price value object

---
+++ video-rental-pricing/src/test/kotlin/pl/michalperlak/videorental/pricing/domain/PriceSpec.kt
@@ -0,0 +1,32 @@
+package pl.michalperlak.videorental.pricing.domain
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import pl.michalperlak.videorental.pricing.domain.domain.Price
+
+class PriceSpec : StringSpec({
+
+    "should sum prices correctly" {
+        // given
+        val price1 = Price.of(10)
+        val price2 = Price.of(20)
+
+        // when
+        val sum = price1 + price2
+
+        // then
+        sum shouldBe Price.of(30)
+    }
+
+    "should multiply price correctly" {
+        // given
+        val factor = 5L
+        val price = Price.of(10)
+
+        // when
+        val result = price * factor
+
+        // then
+        result shouldBe Price.of(50)
+    }
+})
