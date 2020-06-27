829e104614e2905613ff3b4ea9c4544b68b309e7
Michal Perlak <michalperlak@windowslive.com>
2020-06-27 20:33:43 +0200

    Configure build and dependencies

--- video-rental-inventory/build.gradle.kts
+++ video-rental-inventory/build.gradle.kts
@@ -3,11 +3,15 @@ plugins {
 }
 
 val arrowVersion: String by project
+val exposedVersion: String by project
 val kotestVersion: String by project
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("io.arrow-kt", "arrow-core", arrowVersion)
+    implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-java-time", exposedVersion)
 
     testImplementation("io.kotest", "kotest-runner-junit5-jvm", kotestVersion)
     testImplementation("io.kotest", "kotest-assertions-core-jvm", kotestVersion)
--- video-rental-web-api/build.gradle.kts
+++ video-rental-web-api/build.gradle.kts
@@ -1,14 +1,11 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
-    application
+    id("org.springframework.boot") version "2.3.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm")
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    kotlin("plugin.spring")
 }
 
 val arrowVersion: String by project
-val reactorNettyVersion: String by project
-val reactorKotlinVersion: String by project
 val kotestVersion: String by project
 val restAssuredVersion: String by project
 
@@ -17,28 +14,10 @@ dependencies {
 
     implementation(kotlin("stdlib"))
     implementation("io.arrow-kt", "arrow-core", arrowVersion)
-    implementation("io.projectreactor.netty", "reactor-netty", reactorNettyVersion)
-    implementation("io.projectreactor.kotlin", "reactor-kotlin-extensions", reactorKotlinVersion)
+    implementation("org.springframework.boot", "spring-boot-starter-web")
 
     testImplementation("io.kotest", "kotest-runner-junit5-jvm", kotestVersion)
     testImplementation("io.kotest", "kotest-assertions-core-jvm", kotestVersion)
     testImplementation("io.kotest", "kotest-assertions-arrow-jvm", kotestVersion)
     testImplementation("io.rest-assured", "kotlin-extensions", restAssuredVersion)
-}
-
-val mainClass = "pl.michalperlak.videorental.VideoRentalApplicationKt"
-application {
-    mainClassName = mainClass
-}
-
-tasks.withType<ShadowJar>() {
-    manifest {
-        attributes["Main-Class"] = mainClass
-    }
-}
-
-tasks {
-    build {
-        dependsOn(shadowJar)
-    }
 }
