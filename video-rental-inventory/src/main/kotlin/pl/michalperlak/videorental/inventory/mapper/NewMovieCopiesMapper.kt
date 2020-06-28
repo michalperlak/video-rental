96884e2b5d5fdfa5fe560a8ff3a9bdb46737d3df
Michal Perlak <michalperlak@windowslive.com>
2020-06-28 22:17:51 +0200

    New copies should have available status

--- video-rental-inventory/src/main/kotlin/pl/michalperlak/videorental/inventory/mapper/NewMovieCopiesMapper.kt
+++ video-rental-inventory/src/main/kotlin/pl/michalperlak/videorental/inventory/mapper/NewMovieCopiesMapper.kt
@@ -3,6 +3,7 @@ package pl.michalperlak.videorental.inventory.mapper
 import arrow.core.Option
 import pl.michalperlak.videorental.inventory.domain.MovieCopy
 import pl.michalperlak.videorental.inventory.domain.MovieCopyId
+import pl.michalperlak.videorental.inventory.domain.MovieCopyStatus
 import pl.michalperlak.videorental.inventory.domain.MovieId
 import pl.michalperlak.videorental.inventory.dto.NewMovieCopy
 import java.time.Instant
@@ -10,4 +11,4 @@ import java.time.Instant
 internal fun NewMovieCopy.createMovieCopy(copyId: MovieCopyId, additionTimestamp: Instant): Option<MovieCopy> =
     MovieId
         .from(movieId)
-        .map { MovieCopy(copyId, it, additionTimestamp) }
+        .map { MovieCopy(copyId, it, additionTimestamp, MovieCopyStatus.AVAILABLE) }
