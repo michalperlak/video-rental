f83d1b39112d35f2b0aaff62f9a7473179dfafee
Michal Perlak <michalperlak@windowslive.com>
2020-06-30 02:26:17 +0200

    Add toString to ReturnId

--- video-rental-rentals/src/main/kotlin/pl/michalperlak/videorental/rentals/domain/ReturnId.kt
+++ video-rental-rentals/src/main/kotlin/pl/michalperlak/videorental/rentals/domain/ReturnId.kt
@@ -5,6 +5,8 @@ import java.util.UUID
 internal data class ReturnId(
     private val id: UUID
 ) {
+    override fun toString(): String = id.toString()
+
     companion object {
         fun generate(): ReturnId = ReturnId(UUID.randomUUID())
     }
