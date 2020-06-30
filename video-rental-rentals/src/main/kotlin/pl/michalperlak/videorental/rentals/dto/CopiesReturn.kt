aa0c4e430138247ed614ad5cc3cb411d29b487dd
Michal Perlak <michalperlak@windowslive.com>
2020-06-30 11:49:43 +0200

    Add total delay charge

--- video-rental-rentals/src/main/kotlin/pl/michalperlak/videorental/rentals/dto/CopiesReturn.kt
+++ video-rental-rentals/src/main/kotlin/pl/michalperlak/videorental/rentals/dto/CopiesReturn.kt
@@ -1,10 +1,14 @@
 package pl.michalperlak.videorental.rentals.dto
 
 import arrow.core.ListK
+import pl.michalperlak.videorental.pricing.api.Price
 import java.time.LocalDate
 
 data class CopiesReturn(
     val returnId: String,
     val returnDate: LocalDate,
     val items: ListK<ReturnedCopy>
-)
+) {
+    val totalDelayCharge: Price
+        get() = items.fold(Price.ZERO) { acc, returnedCopy -> acc + returnedCopy.delayCharge }
+}
